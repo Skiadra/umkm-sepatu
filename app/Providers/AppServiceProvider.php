7a51b5af92225969e700c6ff09c2b51dafbfac2c
Skiadra <riadywiguna5@gmail.com>
2024-06-06 11:57:52 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,19 +4,22 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
-
-
+     * Register any application services.
      */
     public function register(): void
     {
+        //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        //
         if (config('app.env') != 'local') {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
